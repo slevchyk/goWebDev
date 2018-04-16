c05d0766cbb35681f72395b3598be2062e38c041
slevchyk <s.levchyk@gmail.com>
2018-04-16 18:18:40 +0300

    lesson 05

--- 05data/main.go
+++ 05data/main.go
@@ -9,7 +9,7 @@ import (
 var tpl *template.Template
 
 func init() {
-	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
+	tpl = template.Must(template.ParseGlob("*.gohtml"))
 }
 
 func main() {
