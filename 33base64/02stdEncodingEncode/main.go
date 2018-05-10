24cfd835ce152d93db23cd66cb070d190d95be27
slevchyk <s.levchyk@gmail.com>
2018-05-11 00:47:53 +0300

    lesson 33

---
+++ 33base64/02stdEncodingEncode/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+)
+
+func main() {
+
+	s := `One of the hot topics at Google I/O 2018 has been the evolution of the Material Design.
+		The website has been overhauled and lots of new guidelines have been added, with some of 
+		the changes being discussed in sessions at the conference. Material Theming is also being
+		introduced to help developers implement the design language in their apps while retaining
+		their own style. Another tool, Material Gallery, has just been launched to help designers
+		organize their projects.`
+
+	s64 := base64.StdEncoding.EncodeToString([]byte(s))
+
+	fmt.Println(len(s))
+	fmt.Println(len(s64))
+
+	fmt.Println(s)
+	fmt.Println(s64)
+}
---
+++ 33base64/0302stdEncodingDecode/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+)
+
+func main() {
+
+	s := `One of the hot topics at Google I/O 2018 has been the evolution of the Material Design.
+		The website has been overhauled and lots of new guidelines have been added, with some of 
+		the changes being discussed in sessions at the conference. Material Theming is also being
+		introduced to help developers implement the design language in their apps while retaining
+		their own style. Another tool, Material Gallery, has just been launched to help designers
+		organize their projects.`
+
+	s64 := base64.StdEncoding.EncodeToString([]byte(s))
+
+	dsb, err := base64.StdEncoding.DecodeString(s64)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	ds := string(dsb)
+
+	fmt.Println(len(s))
+	fmt.Println(len(s64))
+	fmt.Println(len(ds))
+
+	fmt.Println(s)
+	fmt.Println(s64)
+	fmt.Println(ds)
+}
